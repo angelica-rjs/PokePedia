177a8fb9ca66509ae7134a6a9aba1ec1a321b2cc
angelica-rjs <angelicarojas.rys@gmail.com>
2023-08-02 15:27:21 -0400

    llamadado de api listo

--- Pokedex/src/App.vue
+++ Pokedex/src/App.vue
@@ -1,47 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+    import { ref } from 'vue';
+    const pokemones = ref(null);
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
+      .then(response => response.json()) 
+      .then((data)=>{
+         pokemones.value = data;
+          pokemones.value.results.forEach(pokemon => {
+            fetch(pokemon.url)
+            .then(subResponse => subResponse.json())
+            .then((subData) =>{ 
+              pokemon.id = subData.id;
+              pokemon.img = subData.sprites.other.dream_world.front_default;
+              pokemon.type = subData.types[0].type.name
+             
+            })
+
+          });
+      } );
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <h1>
+  {{pokemones}}
+ </h1>   -->
+ <ul>
+  <li v-for="pokemon in pokemones.results" >
+    {{pokemon.name}}
+    {{pokemon.id}}
+    {{pokemon.type}}
+    <img v-bind:src="pokemon.img"> 
+  </li>
+ </ul>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
